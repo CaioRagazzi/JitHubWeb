0ec5d7190996d060f60115c87147aa0ce07a01c5
caioragazzi <ca.ragazzi@gmail.com>
2019-05-02 19:00:02 -0300

    adicionando layout

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,56 @@
-<template>
-    <div class="login">
-        <h3>Login</h3>
-        <input type="text" v-model="login" placeholder="Login"><br>
-        <input type="password" v-model="senha" placeholder="Senha"><br>
-        <button @click="logar">Logar</button>
-        <p><router-link to="/signup"> Criar conta </router-link></p>
-    </div>
-</template>
+<!--<template>
+     <div class="login">
+         <h3>Login</h3>
+         <input type="text" v-model="login" placeholder="Login"><br>
+         <input type="password" v-model="senha" placeholder="Senha"><br>
+         <button @click="logar">Logar</button>
+         <p><router-link to="/signup"> Criar conta </router-link></p>
+     </div>
+ </template>-->
+ <template>
+     <div class="row justify-content-center">
+            <div class="col-lg-5 col-md-7">
+                <div class="card bg-secondary shadow border-0">
+                    <div class="card-body px-lg-5 py-lg-5">
 
+                            <base-input class="input-group-alternative mb-3"
+                                        placeholder="Email"
+                                        addon-left-icon="ni ni-email-83"
+                                        v-model="login">
+                            </base-input>
+
+                            <base-input class="input-group-alternative"
+                                        placeholder="Password"
+                                        type="password"
+                                        addon-left-icon="ni ni-lock-circle-open"
+                                        v-model="senha">
+                            </base-input>
+
+                            <div class="text-center">
+                                <base-button type="primary" class="my-4">Sign in</base-button>
+                            </div>
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-6 text-right">
+                        <router-link to="/register" class="text-light"><small>Create new account</small></router-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+ </template>
+ 
+ 
 <script>
 import firebase from 'firebase'
+import BaseInput from '@/components/BaseInput.vue'
+import BaseButton from '@/components/BaseButton.vue'
 
 export default {
+    components: {
+    'base-input': BaseInput,
+    'base-button': BaseButton
+    },
     name: 'login',
     data() {
         return{
